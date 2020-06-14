fa55acf58ef986cf6d99c7c6b2356bb6b6405a2e
duttaditya18@gmail.com <duttaditya18@gmail.com>
2020-06-14 06:12:54 +0530

    added a few instructions

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,18 +1,66 @@
 use crate::ram::Ram;
+use crate::cpu::Cpu;
+use crate::cpu;
 
 pub struct Chip8 {
     ram: Ram,
+    cpu: Cpu
 }
 
+// Related Functions
 impl Chip8 {
     pub fn new() -> Chip8 {
-        Chip8 { ram: Ram::new() }
+        Chip8 {
+            ram: Ram::new(),
+            cpu: Cpu::new()
+        }
+    }
+}
+
+// Methods
+impl Chip8 {
+    pub fn load_rom(&mut self, data: &Vec<u8>) {
+
+        // Load sprites
+        let sprites: [[u8; 5]; 16] = [
+            [0xF0, 0x90, 0x90, 0x90, 0xF0], // 0
+            [0x20, 0x60, 0x20, 0x20, 0x70], // 1
+            [0xF0, 0x10, 0xF0, 0x80, 0xF0], // 2
+            [0xF0, 0x10, 0xF0, 0x10, 0xF0], // 3
+            [0x90, 0x90, 0xF0, 0x10, 0x10], // 4
+            [0xF0, 0x80, 0xF0, 0x10, 0xF0], // 5
+            [0xF0, 0x80, 0xF0, 0x90, 0xF0], // 6
+            [0xF0, 0x10, 0x20, 0x40, 0x40], // 7
+            [0xF0, 0x90, 0xF0, 0x90, 0xF0], // 8
+            [0xF0, 0x90, 0xF0, 0x10, 0xF0], // 9
+            [0xF0, 0x90, 0xF0, 0x90, 0x90], // A
+            [0xE0, 0x90, 0xE0, 0x90, 0xE0], // B
+            [0xF0, 0x80, 0x80, 0x80, 0xF0], // C
+            [0xE0, 0x90, 0x90, 0x90, 0xE0], // D
+            [0xF0, 0x80, 0xF0, 0x80, 0xF0], // E
+            [0xF0, 0x80, 0xF0, 0x80, 0x80]  // F
+        ];
+        for i in 0..80 {
+            self.ram.write_byte(i as u16, sprites[i / 5][i % 5]);
+        }
+
+        // Load game
+        for i in 0..data.len() {
+            self.ram.write_byte(cpu::PROGRAM_START + (i as u16), data[i]);
+        }
     }
 
-    pub fn load_game(&mut self, data: &Vec<u8>) {
-        let program_start = 0x200;
-        for i in 0..data.len() {
-            self.ram.write(program_start + (i as u16), data[i]);
-        }
+    pub fn run_instruction(&mut self) {
+        self.cpu.run_opcode(&mut self.ram);
+    }
+}
+
+// Testing
+impl Chip8 {
+    pub fn test_ram(&self) {
+        println!("{:?}", self.ram);
+    }
+    pub fn test_cpu(&self) {
+        println!("{:?}", self.cpu);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 use std::fs::File;
 use std::io;
 use std::io::Read;
+use chip8::Chip8;
 
 mod chip8;
-mod ram;
+pub mod ram;
+mod cpu;
 
 fn main() -> io::Result<()> {
-    let mut file = File::open("games/INVADERS.ch8")?;
+    let mut file = File::open("games/TETRIS.ch8")?;
     let mut data = Vec::<u8>::new();
-    let _ = file.read_to_end(&mut data);
+    let _ = file.read_to_end(&mut data)?;
 
-    let chip = chip8::Chip8::new();
+    let mut chip8 = Chip8::new();
+    chip8.load_rom(&data);
 
-    println!("{:?}", data);
+    chip8.test_cpu();
+    // chip8.test_ram();
+
+    loop {
+        chip8.run_instruction()
+    }
+
     Ok(())
 }
